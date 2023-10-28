af29202cca9f8cb51bcd8d90e0a80df3fed284d2
William Marques <william.marques@pagar.me>
2023-10-28 10:26:35 -0300

    Initial commit

---
+++ cmd/ordersystem/wire.go
@@ -0,0 +1,51 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"database/sql"
+
+	"github.com/google/wire"
+	"github.com/willcmarques/goexpert-desafio-clean-arch/internal/entity"
+	"github.com/willcmarques/goexpert-desafio-clean-arch/internal/event"
+	"github.com/willcmarques/goexpert-desafio-clean-arch/internal/infra/database"
+	"github.com/willcmarques/goexpert-desafio-clean-arch/internal/infra/web"
+	"github.com/willcmarques/goexpert-desafio-clean-arch/internal/usecase"
+	"github.com/willcmarques/goexpert-desafio-clean-arch/pkg/events"
+)
+
+var setOrderRepositoryDependency = wire.NewSet(
+	database.NewOrderRepository,
+	wire.Bind(new(entity.OrderRepositoryInterface), new(*database.OrderRepository)),
+)
+
+var setEventDispatcherDependency = wire.NewSet(
+	events.NewEventDispatcher,
+	event.NewOrderCreated,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
+	wire.Bind(new(events.EventDispatcherInterface), new(*events.EventDispatcher)),
+)
+
+var setOrderCreatedEvent = wire.NewSet(
+	event.NewOrderCreated,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
+)
+
+func NewCreateOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.CreateOrderUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		usecase.NewCreateOrderUseCase,
+	)
+	return &usecase.CreateOrderUseCase{}
+}
+
+func NewWebOrderHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebOrderHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		web.NewWebOrderHandler,
+	)
+	return &web.WebOrderHandler{}
+}
