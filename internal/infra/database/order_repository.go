3c53913bcf4af64448619fbc61b017022535a20c
William Marques <william.marques@pagar.me>
2023-10-28 11:42:14 -0300

    Criação do ListOrders

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -34,3 +34,22 @@ func (r *OrderRepository) GetTotal() (int, error) {
 	}
 	return total, nil
 }
+
+func (r *OrderRepository) ListOrders() ([]entity.Order, error) {
+	rows, err := r.Db.Query("Select id, price, tax, final_price from orders")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var orders []entity.Order
+	for rows.Next() {
+		var order entity.Order
+		err = rows.Scan(&order.ID, &order.Price, &order.Tax, &order.FinalPrice)
+		if err != nil {
+			panic(err)
+		}
+		orders = append(orders, order)
+	}
+	return orders, nil
+}
--- internal/infra/database/order_repository_test.go
+++ internal/infra/database/order_repository_test.go
@@ -16,14 +16,26 @@ type OrderRepositoryTestSuite struct {
 	Db *sql.DB
 }
 
-func (suite *OrderRepositoryTestSuite) SetupSuite() {
+func (suite *OrderRepositoryTestSuite) setup() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	suite.NoError(err)
 	db.Exec("CREATE TABLE orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
 	suite.Db = db
 }
 
+func (suite *OrderRepositoryTestSuite) SetupSuite() {
+	suite.setup()
+}
+
+func (suite *OrderRepositoryTestSuite) SetupTest() {
+	suite.setup()
+}
+
 func (suite *OrderRepositoryTestSuite) TearDownTest() {
+	suite.Db.Close()
+}
+
+func (suite *OrderRepositoryTestSuite) TearDownSuite() {
 	suite.Db.Close()
 }
 
@@ -48,4 +60,18 @@ func (suite *OrderRepositoryTestSuite) TestGivenAnOrder_WhenSave_ThenShouldSaveO
 	suite.Equal(order.Price, orderResult.Price)
 	suite.Equal(order.Tax, orderResult.Tax)
 	suite.Equal(order.FinalPrice, orderResult.FinalPrice)
+}
+
+func (suite *OrderRepositoryTestSuite) TestShouldReturnAllOrders() {
+	stmt, err := suite.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
+	suite.NoError(err)
+	defer stmt.Close()
+	stmt.Exec("1", 10.0, 0.1, 10.1)
+	stmt.Exec("2", 20.0, 0.2, 20.2)
+	stmt.Exec("3", 30.0, 0.3, 30.3)
+
+	repo := NewOrderRepository(suite.Db)
+	orders, err := repo.ListOrders()
+	suite.NoError(err)
+	suite.Equal(3, len(orders))
 }
