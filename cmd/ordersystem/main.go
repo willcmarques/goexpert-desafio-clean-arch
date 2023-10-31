fe6e663f7b5f46fcc973d7397b51384da0b76e67
William Marques <william.marques@pagar.me>
2023-10-31 09:55:49 -0300

    Implementando o listOrders no web

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -48,6 +48,7 @@ func main() {
 	webserver := webserver.NewWebServer(configs.WebServerPort)
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
 	webserver.AddHandler("/order", webOrderHandler.Create)
+	webserver.AddHandler("/orders", webOrderHandler.List)
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
 
